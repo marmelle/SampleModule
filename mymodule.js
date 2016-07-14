0b5b6f6ebbb43941bf2432198abf4b8be54fb32c
marmelle <margaritaniluag2@gmail.com>
2016-07-14 19:02:53 +0800

    My Sample Module

---
+++ mymodule.js
@@ -0,0 +1,14 @@
+function area(length,width){
+    return length * width;
+    
+}
+
+function perimeter(length,width){
+    return (length*width) * 2;
+}
+
+module.exports = {
+    
+    computeArea :area,
+    computePerimeter :perimeter
+};
